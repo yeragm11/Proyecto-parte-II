08f8eb5ca3b256e4ec0f707ae116dce38ce18862
yeragm11 <88159474+yeragm11@users.noreply.github.com>
2021-08-22 16:38:42 -0600

    Add files via upload

---
+++ Proyecto de vida, parte II/scripts/carrito.js
@@ -0,0 +1,21 @@
+﻿class Carrito{
+
+	//Añadir producto
+	comprarProducto(e){
+		e.prevDefault();
+		if (e.target.classList.contains("agregar-carrito")) {
+
+			const producto = e.target.parentElement.parentElement;
+			this.leerDatosProductos(producto);
+		}
+	}
+
+	leerDatosProducto(producto){
+
+		const infoProducto = {
+			nombre : producto.querySelector('nombre')
+			imagen : producto.querySelector('imagen').src,
+		}
+	}
+
+}
